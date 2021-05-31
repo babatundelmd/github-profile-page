7803db6e2290dce3757d1f70205de2316791d5ef
Babatunde Lamidi <odunayohlamidi@gmail.com>
2021-05-31 17:13:48 +0100

    add pull request count

--- profile.js
+++ profile.js
@@ -16,6 +16,8 @@ async function getRequest() {
     // Get Data from GITHUB
     const getReposUrl = req.repos_url;
     const reposUrl = await (await fetch(getReposUrl)).json();
+    console.log(reposUrl);
+
     reposUrl.forEach((data) => {
       const li = document.createElement('li');
       let obj = data;
@@ -48,11 +50,27 @@ async function getRequest() {
         ></path></svg>${obj.stargazers_count}
     </span>
 
+    <span class="_m_r">
+    <svg
+    aria-label="forks"
+    role="img"
+    viewBox="0 0 16 16"
+    version="1.1"
+    data-view-component="true"
+    height="16"
+    width="16"
+    class="nav-header-svg"
+    >
+    <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+    </svg>${obj.forks_count}
+
+    </span>
 
 
 
 
-    <span>Updated on ${getDay} ${getShortMonth}</span>
+
+  <span>Updated on ${getDay} ${getShortMonth}</span>
   </p>
   </div>
   <div class="button">
